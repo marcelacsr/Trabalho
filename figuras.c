57bb084413bce11a59e486081a7d529e9ea5d20b
Marcela Ramos <marcelacsr@id.uff.br>
2019-05-28 20:09:08 -0300

    iniciado figuras

---
+++ figuras.c
@@ -0,0 +1,24 @@
+typedef struct triangulo {
+	float base, altura;
+	float area;
+} TTRI;
+
+typedef struct circulo {
+	float dim;
+	float area;
+} TCIR;
+
+typedef struct quadrado {
+	float lado;
+	float area;
+} TQUA;
+
+typedef struct trapezio {
+	float base_menor, base_maior, altura;
+	float area;
+} TTRA;
+
+typedef struct retangulo {
+	float base, altura;
+	float area;
+} TRET;
