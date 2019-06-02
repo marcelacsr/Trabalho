20a9ee710336a6c3c4aa21a495d505009c1b9b55
Wilker <Wilker.rj@gmail.com>
2019-06-02 14:23:42 -0300

    refatora nome de método

--- figuras.c
+++ figuras.c
@@ -18,39 +18,39 @@ typedef struct retangulo {
 	float base, altura;
 } TRET;
 
-TCIR* criar_circulo(float raio);
-TQUA* criar_quadrado(float lado);
-TTRI* criar_triangulo(float base, float altura);
-TRET* criar_retangulo(float base, float altura);
-TTRA* criar_trapezio(float base_menor, float base_maior, float altura);
+TCIR* cria_circulo(float raio);
+TQUA* cria_quadrado(float lado);
+TTRI* cria_triangulo(float base, float altura);
+TRET* cria_retangulo(float base, float altura);
+TTRA* cria_trapezio(float base_menor, float base_maior, float altura);
 
-TCIR* criar_circulo(float raio){
+TCIR* cria_circulo(float raio){
 	TCIR* novo = (TCIR*) malloc(sizeof(TCIR));
 	novo->raio = raio;
 	return novo;
 }
 
-TQUA* criar_quadrado(float lado){
+TQUA* cria_quadrado(float lado){
 	TQUA* novo = (TQUA*) malloc(sizeof(TQUA));
 	novo->lado = lado;
 	return novo;
 }
 
-TTRI* criar_triangulo(float base, float altura){
+TTRI* cria_triangulo(float base, float altura){
 	TTRI* novo = (TTRI*) malloc(sizeof(TTRI));
 	novo->base = base;
 	novo->altura = altura;
 	return novo;
 }
 
-TRET* criar_retangulo(float base, float altura){
+TRET* cria_retangulo(float base, float altura){
 	TRET* novo = (TRET*) malloc(sizeof(TRET));
 	novo->base = base;
 	novo->altura = altura;
 	return novo;
 }
 
-TTRA* criar_trapezio(float base_menor, float base_maior, float altura){
+TTRA* cria_trapezio(float base_menor, float base_maior, float altura){
 	TTRA* novo = (TTRA*) malloc(sizeof(TTRA));
 	novo->base_menor = base_menor;
 	novo->base_maior = base_maior;
