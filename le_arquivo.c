0eb8df7148c9a5bc41b8733bd672e08f142277d4
Wilker <Wilker.rj@gmail.com>
2019-06-04 12:14:03 -0300

    Removendo prints da leitura do arquivo

--- le_arquivo.c
+++ le_arquivo.c
@@ -13,13 +13,10 @@ TAG* read_arquivo(TAG* arv){
         char fig[4];
         int t = scanf("%d/%d/%s",&a, &b, fig);
         if(t<=0) break;
-        printf("Conteúdo do arquivo:\n a: %d b: %d figura: %s\n",a, b, fig);
-        printf("a = %d b = %d fig = %s\n",a, b, fig);
         void *figura = NULL;
         if(!strcmp(fig,"TRI")){
             int base = 0, altura = 0;
             scanf(" %d %d",&base,&altura);
-            printf("base: %d altura: %d\n",base,altura);
             figura = (void *)cria_triangulo(base, altura);
             tipo = 2;
             imprime_triangulo(figura);
@@ -27,7 +24,6 @@ TAG* read_arquivo(TAG* arv){
         if(!strcmp(fig,"RET")){
             int base = 0, altura = 0;
             scanf(" %d %d",&base,&altura);
-            printf("base: %d altura: %d\n",base,altura);
             figura = (void *)cria_retangulo(base, altura);
             tipo = 3;
             imprime_retangulo(figura);
@@ -35,7 +31,6 @@ TAG* read_arquivo(TAG* arv){
         if(!strcmp(fig,"TRA")){
             int base_menor = 0, base_maior = 0, altura = 0;
             scanf(" %d %d %d",&base_maior, &base_menor, &altura);
-            printf("base_maior = %d base_menor = %d  altura = %d\n",base_maior, base_menor,altura);
             figura = (void *)cria_trapezio(base_menor,base_maior,altura);
             tipo = 4;
             imprime_trapezio(figura);
@@ -43,7 +38,6 @@ TAG* read_arquivo(TAG* arv){
         if(!strcmp(fig,"CIR")){
             int raio = 0;
             scanf(" %d",&raio);
-            printf("raio = %d \n",raio);
             figura = (void *)cria_circulo(raio);
             tipo = 0;
             imprime_circulo(figura);
@@ -51,7 +45,6 @@ TAG* read_arquivo(TAG* arv){
         if(!strcmp(fig,"QUA")){
             int lado = 0;
             scanf(" %d",&lado);
-            printf("lado = %d \n",lado);
             figura = (void *)cria_quadrado(lado);
             tipo = 1;
             imprime_quadrado(figura);
